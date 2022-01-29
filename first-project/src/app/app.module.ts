e8671497163495d7354478773b90a3fe03e71568
José Eduardo <jose.eduardo1201@outlook.com>
2022-01-29 00:26:38 -0300

    10 - Class and Style binding, ngx-bootstrap

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesModule } from './courses/courses.module';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { StyleBindingComponent } from './style-binding/style-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    StyleBindingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    CoursesModule
+    CoursesModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
