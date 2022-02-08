800738ff886485850ceaeeac6867c4cdc055198f
José Eduardo <jose.eduardo1201@outlook.com>
2022-02-08 20:15:32 -0300

    29 - Directive ngClass

---
+++ directives/src/app/directive-ngclass/directive-ngclass.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-ngclass',
+  templateUrl: './directive-ngclass.component.html',
+  styleUrls: ['./directive-ngclass.component.css']
+})
+export class DirectiveNgclassComponent implements OnInit {
+
+  myFavorite: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onClick() {
+    this.myFavorite = !this.myFavorite;
+  }
+}
